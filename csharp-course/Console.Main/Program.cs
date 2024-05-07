aec44495bee41ceb7442e2a54dee833bac4eac36
jdhdev04 <52967111+jdhdev04@users.noreply.github.com>
2024-05-07 14:42:18 -0300

    First Commit!

---
+++ csharp-course/Console.Main/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
